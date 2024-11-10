e52228001c8148b5adb790377b5aa95639b1ea7d
mcimam <imam123.ci@gmail.com>
2024-11-10 20:23:33 +0700

    [ADD] add pos simulation module

---
+++ pos_simulation/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "POS Simulation",
+    "version": "17.0.1.0",
+    "summary": "Module to simulate Point of Sales sessions and orders",
+    "category": "Point of Sale",
+    "author": "Sedari Coffe, Choirul Imam",
+    "depends": ["point_of_sale"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/pos_simulation_views.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+}
