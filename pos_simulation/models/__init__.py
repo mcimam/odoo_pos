42c7d5e3f9829559e909d7b38f183169b4e19c34
mcimam <imam123.ci@gmail.com>
2024-11-10 21:26:47 +0700

    [DEL] Remove unused module

--- pos_simulation/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import pos_order
-from . import pos_simulation
-from . import pos_simulation_product
--- pos_simulation/models/pos_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class PosOrder(models.Model):
-    _inherit = "pos.order"
-
-    simulation_id = fields.Many2one("pos.simulation", ondelete="set null", copy=False)
